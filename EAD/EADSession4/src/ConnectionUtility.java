540cada525acacbc3ba662c392df6410834170c7
Pranjal Agrawal <pranjal.agrawal@metacube.com>
2020-02-18 12:07:56 +0530

    Parking system

---
+++ EAD/EADSession4/src/ConnectionUtility.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+
+public class ConnectionUtility 
+{
+	public static Connection con;
+	
+	public static Connection getConnection()
+	{
+		
+		if(con==null)
+		{
+			try
+			{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/MetacubeParking","root","root");
+						
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		
+		return con;
+	}
+	
+
+}
